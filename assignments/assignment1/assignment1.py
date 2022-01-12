416ee3edc60dfc2ab51247487db3c87000656af1
InsTincT-76 <89951041+InsTincT-76@users.noreply.github.com>
2022-01-12 15:24:00 -0500

    Added assignment 1

---
+++ assignments/assignment1/assignment1.py
@@ -0,0 +1,7 @@
+# Author: Ahmed Altouqi
+
+print("My Full name is Ahmed Altouqi")
+
+print("I am from Oman and Arabic is actually my first language.")
+
+print("My favorite thing to do has to be either playing video games, playing soccer, or reading a book.")
