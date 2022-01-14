3c1a0f25be61edda696aa397c59eea5046d4c7bf
InsTincT-76 <89951041+InsTincT-76@users.noreply.github.com>
2022-01-13 20:49:26 -0500

    Add files via upload

---
+++ exercises/Jan11/string-input.py
@@ -0,0 +1,7 @@
+# Getting data from the user
+#Author: Ahmed Al touqi
+
+first_name = input("Enter first name: ")
+last_name = input("Enter last name: ")
+
+print(f"Welcome {first_name} {last_name}! ")
---
+++ exercises/Jan11/strings-vars.py
@@ -0,0 +1,14 @@
+#Author: Ahmed Al touqi
+
+
+first_name = "Ahmed"
+last_name = "Al touqi"
+print("Welcome " + first_name + "" + last_name + "!" )
+
+print(f"Welcome {first_name} {last_name}!")
+
+print("I love cheese\nBut I don't like milk.")
+
+print(f"""I love cheese
+ But I don't like milk.""")
+
