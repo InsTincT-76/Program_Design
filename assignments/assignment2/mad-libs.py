94bf108477c160282d87f5aca81e060c0e48e994
InsTincT-76 <89951041+InsTincT-76@users.noreply.github.com>
2022-01-18 18:30:23 -0500

    Add files via upload

---
+++ assignments/assignment2/mad-libs.py
@@ -0,0 +1,15 @@
+# Author: Ahmed Altouqi
+first_number = input("First Number: ")
+second_number = input("Second Number: ")
+animal = input("Animal: ")
+liquid = input("Liquid: ")
+third_number = input("Third Number: ")
+first_adjective = input("First Adjective: ")
+sweet_food = input("Sweet Food: ")
+forth_number = input("Forth Number ")
+second_adjective = input("Second Adjective: ")
+
+
+print(f"""First, preheat the oven to {first_number} degrees. Then beat {second_number} {animal} egg(s) with a cup of {liquid}.
+Next add {third_number} teaspoon(s) of baking soda and mix with eggs and {liquid} until it reaches a {first_adjective} texture.
+Don't forget the {sweet_food}! Pour into a 9 by 9 pan and bake for {forth_number} minute(s) or until the cake is {second_adjective}.""")
